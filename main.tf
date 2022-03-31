089b77090a4f8b819de95b63d9f02b5bc3e48d3e
Darkirondan <dev.darkirondan@gmail.com>
2022-03-30 19:30:45 -0600

    v1

---
+++ main.tf
@@ -0,0 +1,12 @@
+data "azurerm_resource_group" "myterraformresource" {
+  name     = "rg-central-training"
+  # location = "Central US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "myvirtualnetwork" {
+  name                = var.nameVN
+  resource_group_name = data.azurerm_resource_group.myterraformresource.name
+  location            = data.azurerm_resource_group.myterraformresource.location
+  address_space       = ["10.0.0.0/16"]
+}
---
+++ varaibles.tf
@@ -0,0 +1,4 @@
+variable "nameVN" {
+  default     = "Dan VN"
+  description = "name of virtual network"
+}
